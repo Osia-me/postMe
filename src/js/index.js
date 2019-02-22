0841a086baebbd8b272bfcb3c1e0e56b665ec173
Osia-me <olga.melnykovych@gmail.com>
2019-02-22 12:16:26 +0100

    remove post functionality

--- src/js/index.js
+++ src/js/index.js
@@ -6,13 +6,17 @@ document.addEventListener('DOMContentLoaded', getPosts);
 
 //Listen for add post event
 document.querySelector('.post-submit').addEventListener('click', createPost);
+//Listen for delete
+document.querySelector('#posts').addEventListener('click', deletePost);
 
+
+//Get posts
 function getPosts(){
   http.get('http://localhost:3000/posts')
   .then(data => ui.showPost(data))
   .catch(err => console.log(err));
 }
-
+//Create posts
 function createPost(){
   const title = document.querySelector('#title').value;
   const body = document.querySelector('#body').value;
@@ -29,4 +33,19 @@ function createPost(){
       getPosts();
     })
     .catch(err => console.log(err));
+}
+
+function deletePost(e){
+  e.preventDefault();
+  if(e.target.parentElement.classList.contains('delete')){
+    const id = e.target.parentElement.dataset.id;
+      if(confirm('Are you sure?')){
+        http.delete(`http://localhost:3000/posts/${id}`)
+        .then(data => {
+          ui.ShowAlert('Post removed', 'alert alert-success');
+          getPosts();
+        })
+        .catch(err => console.log(err));
+      }
+  }
 }
